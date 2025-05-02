acb625c738c2c38014f48d32a12e193de34bf355
michaelhyi <michaelyi2004@gmail.com>
2025-05-01 21:53:43 -0400

    updated docker bash script to reuse builders

--- docker.sh
+++ docker.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-docker buildx create --use
+if ! docker buildx inspect pipelined-vm-builder > /dev/null 2>&1; then
+    docker buildx create --name pipelined-vm-builder --use
+else
+    docker buildx use pipelined-vm-builder
+fi
+
 docker buildx build --platform=linux/amd64 -t pipelined-vm . --load
 
 docker run --platform=linux/amd64 -it --rm \
 	-v "$PWD":/workspace \
 	-w /workspace \
 	pipelined-vm bash
-
