6df695964e289f3467ed7a5c1f4579da900389b1
michaelhyi <michaelyi2004@gmail.com>
2025-05-04 18:11:58 -0700

    added preliminary code for mem stage

--- src/stages/mem.c
+++ src/stages/mem.c
@@ -9,27 +9,46 @@
 #include "../vm.h"
 
 void *mem_run(void *arg) {
-    if (!arg) {
-        fprintf(stderr, "mem_run failed: arg must be non-null\n");
-        pthread_exit((void *)(intptr_t)EINVAL);
-    }
+    (void)arg;
 
+    pthread_mutex_lock(&vm.ebuf_mutex);
     ebuf_t ebuf = vm.ebuf;
+    vm.ebuf.read = 1;
+    pthread_cond_signal(&vm.ebuf_read_cond);
+    pthread_mutex_unlock(&vm.ebuf_mutex);
 
     if (!ebuf.ready) {
         return NULL;
     }
 
-    mbuf_t *mbuf = (mbuf_t *)arg;
-    memcpy(mbuf, &ebuf, sizeof(mbuf_t));
+    ebuf.read = 1;
+
+    mbuf_t mbuf;
+    mbuf.ready = 1;
+    mbuf.read = 0;
+    mbuf.pc = ebuf.pc;
+    mbuf.opcode = ebuf.opcode;
+    mbuf.result = ebuf.result;
+    mbuf.reg = ebuf.reg;
 
     if (ebuf.opcode == OP_LD || ebuf.opcode == OP_LDR) {
-        mbuf->result = vm.mem[ebuf.result];
-    } else if (ebuf.opcode == OP_LDI) {
-        // TODO: send bubbles for STI too
+        pthread_mutex_lock(&vm.mem_mutex);
+        mbuf.result = vm.mem[(uint16_t)ebuf.result];
+        pthread_mutex_unlock(&vm.mem_mutex);
     } else if (ebuf.opcode == OP_ST || ebuf.opcode == OP_STR) {
-        vm.mem[ebuf.result] = ebuf.reg;
+        pthread_mutex_lock(&vm.mem_mutex);
+        vm.mem[(uint16_t)ebuf.result] = ebuf.reg;
+        pthread_mutex_unlock(&vm.mem_mutex);
+    } else if (ebuf.opcode == OP_LDI || ebuf.opcode == OP_STI) {
+        // TODO
     }
+
+    pthread_mutex_lock(&vm.mbuf_mutex);
+    while (!vm.mbuf.read) {
+        pthread_cond_wait(&vm.mbuf_read_cond, &vm.mbuf_mutex);
+    }
+    vm.mbuf = mbuf;
+    pthread_mutex_unlock(&vm.mbuf_mutex);
 
     return NULL;
 }
